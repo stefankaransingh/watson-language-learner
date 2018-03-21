860c8b435a490ac46861eed8d66ade9ba35fdbe9
stefankaransingh <stefankaransingh@gmail.com>
2018-03-21 12:58:22 +0000

    Added a fix for loading word using absolute path

--- load_words.py
+++ load_words.py
@@ -1,6 +1,8 @@
+import os
+
 def get_words():
     words = []
-    with open('words.txt','r') as f:
+    with open(os.path.abspath('words.txt'),'r') as f:
         for word in f:
             words.append(word.rstrip('\n'))
     return words
