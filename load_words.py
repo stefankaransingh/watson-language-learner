43cd925aec693e86c4598c8f6687baa56f02bf53
stefankaransingh <stefankaransingh@gmail.com>
2018-03-22 11:19:54 +0000

    Update load words, fixed loading file error

--- load_words.py
+++ load_words.py
@@ -1,8 +1,11 @@
 import os
 
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+words_file = os.path.join(THIS_FOLDER, 'words.txt')
+
 def get_words():
     words = []
-    with open(os.path.realpath('words.txt'),'r') as f:
+    with open(words_file,'r') as f:
         for word in f:
             words.append(word.rstrip('\n'))
     return words
