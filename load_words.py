ee5f1177b31b900c12c2daa2abe8b9da552718ba
stefankaransingh <stefankaransingh@gmail.com>
2018-03-22 10:42:03 +0000

    updated load_words to use relative path to load words.txt

--- load_words.py
+++ load_words.py
@@ -2,7 +2,7 @@ import os
 
 def get_words():
     words = []
-    with open(os.path.abspath('words.txt'),'r') as f:
+    with open(os.path.realpath('words.txt'),'r') as f:
         for word in f:
             words.append(word.rstrip('\n'))
     return words
